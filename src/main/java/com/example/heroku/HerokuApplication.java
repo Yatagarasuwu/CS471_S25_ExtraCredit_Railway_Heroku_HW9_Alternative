59842f3185eabd5802e047113934167131d4a882
Sabastian Leeper <sabastianleeper@u.boisestate.edu>
2025-05-03 14:15:57 -0600

    commiting from intellij weirdge

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -33,10 +33,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 @Controller
 @SpringBootApplication
 public class HerokuApplication {
+
+  private static final String ALPHANUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  private static final Random   RANDOM  = new Random();
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
@@ -57,13 +61,18 @@ public class HerokuApplication {
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+
+      ResultSet rs = stmt.executeQuery(
+              "SELECT tick, random_string FROM table_timestamp_and_random_string"
+      );
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") +
+                " / " +
+                rs.getString("random_string"));
       }
 
       model.put("records", output);
@@ -73,6 +82,15 @@ public class HerokuApplication {
       return "error";
     }
   }
+
+  private String getRandomString() {
+    StringBuilder sb = new StringBuilder(30);
+    for (int i = 0; i < 30; i++) {
+      sb.append(ALPHANUM.charAt(RANDOM.nextInt(ALPHANUM.length())));
+    }
+    return sb.toString();
+  }
+
 
   @Bean
   public DataSource dataSource() throws SQLException {
@@ -86,3 +104,4 @@ public class HerokuApplication {
   }
 
 }
+
